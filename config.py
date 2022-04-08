f6159085418bd97b8e5b19ddcb4565092e85ccb1
cqztc <82072737+cqztc@users.noreply.github.com>
2022-04-08 16:13:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+customhost = "employee.cjvcbowyawpi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "justinthamjiawei-employee"
+customregion = "us-east-1"
+cloudfront = "da93lzgvtn5yz.cloudfront.net"
+
+
